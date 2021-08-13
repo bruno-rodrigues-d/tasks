576ced0121eaa7f8d923342893031b2005c91f22
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2021-08-13 10:51:52 -0300

    Quarto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import Tasks from './components/Tasks';
 
 import './App.css';
 import AddTask from './components/AddTask';
+import { fromCallback } from 'bluebird';
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -23,10 +26,20 @@ const App = () => {
     }
   ]);
 
+  const handleTaskAddition = (taskTitle) => {
+    const newTasks = [... tasks, {
+      title: taskTitle,
+      id: uuidv4(),
+      completed: false
+    }];
+
+    setTasks(newTasks);
+  };
+
   return (
     <>
       <div className="container">
-        <AddTask/>
+        <AddTask handleTaskAddition={handleTaskAddition}/>
         <Tasks tasks={tasks}/>  
       </div>;
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import "./Button.css";
 
-const Button = (children, onClick) => {
+const Button = ({children, onClick}) => {
     return ( 
         <button onClick={onClick} className="button">
             {children}
